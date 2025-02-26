2b49bcdc077234b82d1a44ec6049e3a8a9205e4f
Daniel Osagie <93149465+danielosagie@users.noreply.github.com>
2025-02-25 20:22:40 -0500

    trying to fix build errors

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
 };
 
